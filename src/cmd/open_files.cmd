29e594195d75ed84a86a95bb915ba2a7d2ed3f6d
Hilly Steinmetz <hsteinm@uw.edu>
2023-01-14 14:53:42 -0800

    cmd, script and logger for xml opener

---
+++ src/cmd/open_files.cmd
@@ -0,0 +1,9 @@
+executable            = scripts/open_files.sh 
+getenv                = true
+output                = preprocess/open_files.out
+error                 = preprocess/open_files.error
+log                   = preprocess/open_files.log
+transfer_executable   = false
+request_memory        = 2*1024
+stream_output         = True
+queue
---
+++ src/scripts/open_files.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/nopt/python-3.6/bin/python3 preprocess/open_all_files.py
