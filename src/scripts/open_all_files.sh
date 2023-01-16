63379f5a11e4b76396b5bd16474a596067f798b6
Junyin Chen <VulpesVulpes825@gmail.com>
2023-01-15 17:01:19 -0800

    feat: Add D1.cmd and allow parsing all xml

--- src/scripts/open_all_files.sh
+++ src/scripts/open_all_files.sh
@@ -3,3 +3,5 @@
 dir="/dropbox/22-23/575x/Data/Documents"
 
 src/scripts/./proc_docset.sh "$dir"/devtest/GuidedSumm10_test_topics.xml output/devtest
+src/scripts/./proc_docset.sh "$dir"/training/2009/UpdateSumm09_test_topics.xml output/training
+src/scripts/./proc_docset.sh "$dir"/evaltest/GuidedSumm11_test_topics.xml output/evaltest
