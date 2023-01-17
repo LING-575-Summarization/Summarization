53713eecbcf902ca97c7fb0c8c51d3f79e16ae6b
Hillel (Hilly) Steinmetz <63203563+hasteinmetz@users.noreply.github.com>
2023-01-16 16:04:15 -0800

    Use NLTK tokenizer (#15)
    * feat: Using nltk tokenizer for sentence tokenizing
    * feat: explicit category info
    * fix: Move D1.cmd to root directory per D1 spec
    
    Co-authored-by: batraa <batraa@patas.ling.washington.edu>
    Co-authored-by: Junyin Chen <VulpesVulpes825@gmail.com>

--- src/preprocess/proc_docset.py
+++ src/preprocess/proc_docset.py
@@ -9,10 +9,10 @@ It uses the processing functions defined in read_xml
 import logging
 import os
 import sys
+import re
 import xml.etree.ElementTree as ET
 from typing import Dict, Tuple, List
 from pathlib import Path
-import spacy
 
 
 from get_data_path import resolve_path
@@ -26,14 +26,35 @@ logging.basicConfig(
 logger = logging.getLogger()
 
 
+def get_categories(category_file: Path) -> Dict[str, str]:
+    '''
+    Obtain a dictionary of category names to category files
+    '''
+    with open(category_file, 'r') as infile:
+        lines = infile.readlines()
+    _categories = [l for l in lines if re.match(r'^\d\..*:$', l)]
+    categories = {}
+    for c in _categories:
+        m = re.split(r'(?<=\d)\. ', c)
+        categories[m[0]] = m[1][:-2]
+    return categories
+
+
 def get_data_dir(file: str) -> Dict[str, List[Tuple[str, str, int, int]]]:
     """
     Go through the metadata file and acquire a dictionary of file codes to text.
     Args:
         - file: the metadata .xml file of the test split
     """
+    # open XML file
     with open(file, 'r') as document:
         doc_as_string = document.read()
+    # check if the category file exists or not
+    category_file = os.path.join(os.path.dirname(file), "categories.txt")
+    if os.path.exists(category_file):
+        categories = get_categories(category_file)
+    else:
+        categories = None
     documents_list = ET.fromstring(doc_as_string)
     # Only read docsetA per spec
     topic_nodes = documents_list.findall("topic")
@@ -46,9 +67,10 @@ def get_data_dir(file: str) -> Dict[str, List[Tuple[str, str, int, int]]]:
         path_dict[docset_id] = []
         for document in docset_node.findall("doc"):
             doc_id = document.get("id")
+            doc_category = categories[category] if category else "None"
             try:
                 data_path, corpus_category = resolve_path(doc_id)
-                path_dict[docset_id].append((data_path, doc_id, corpus_category, category))
+                path_dict[docset_id].append((data_path, doc_id, corpus_category, doc_category))
             except FileNotFoundError:
                 logger.warning(
                     "Couldn't load file ID: {}. Issue with path: {}?".format(doc_id, resolve_path(doc_id))
@@ -67,15 +89,19 @@ def write_outputs(path_dict: Dict[str, List[Tuple[str, str, int, int]]], output_
     in a docset
     """
     Path(output_dir).mkdir(parents=True, exist_ok=True)
-    nlp = spacy.load("en_core_web_sm")
     for docset, value in path_dict.items():
         docset_dir = os.path.join(output_dir, docset)
         if not os.path.exists(docset_dir):
             os.mkdir(docset_dir)
         for data_path, doc_id, corpus_type, category_id in value:
             output_path = os.path.join(docset_dir, doc_id)
-            read_by_corpus_type(data_path, doc_id, category_id, corpus_type, output_path, nlp)
+            read_by_corpus_type(data_path, doc_id, category_id, corpus_type, output_path)
     logger.info("Successfully wrote dictionary to files")
+
+
+def main(input_xml_file: Path, output: Path):
+    dict = get_data_dir(input_xml_file)
+    write_outputs(dict, output)
 
 
 if __name__ == '__main__':
@@ -98,5 +124,8 @@ if __name__ == '__main__':
         logger.info("\n======= Script session %s/%s %s:%s:%s =======\n" % now)
         for hndlr in logger.handlers:
             hndlr.setFormatter(logging.Formatter(default_fmt))
-        dict = get_data_dir(input_xml_file)
-        write_outputs(dict, output)
+
+        try:
+            main(input_xml_file, output)
+        except:
+            logger.error("Oops. Encountered an error while running the script")
