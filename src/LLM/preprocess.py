ebbffb9f4d02a9e608bd86bf0042dd2c149b2033
Junyin Chen <vulpesvulpes825@gmail.com>
2023-01-28 22:22:57 -0800

    fix: Limit maximum input

--- src/LLM/preprocess.py
+++ src/LLM/preprocess.py
@@ -1,6 +1,7 @@
 import random
 
 import json
+from nltk.tokenize import word_tokenize
 from rouge_score import rouge_scorer
 
 
@@ -21,29 +22,49 @@ def build_dataset(input_dict, dataset_type):
         output_dict["id"] = docset_id
         # output_dict["title"] = docset["title"]
         input_texts = docset["text"]
-        output_dict["text"] = []
+        output_dict["text"] = ""
+        total_length = 0
+        over_limit = False
         for input_text in input_texts:
-            if dataset_type == "training":
-                output_dict["text"].append(mask_sentences(input_text))
-            else:
-                output_dict["text"].append(" \n ".join(input_text))
+            if over_limit:
+                break
+            new_doc, total_length, over_limit = mask_sentences(input_text, dataset_type, total_length, over_limit)
+            output_dict["text"] = output_dict["text"] + " <|endoftext|> " + new_doc
+        print(total_length)
+        print(output_dict["text"])
         output_dict["summary"] = docset["summary"][random.randint(0, len(docset["summary"]) - 1)]
         output.append(output_dict)
     return output
 
 
-def mask_sentences(input_text):
+def mask_sentences(input_text, dataset_type, total_length, over_limit):
     scores = get_sentence_score(input_text)
     indexes = generate_index_list(len(input_text))
-    top_30_percent = len(input_text) * 3 // 10
-    compare = [x for _, x in sorted(zip(scores, indexes))][:top_30_percent]
+    top_30 = [x for _, x in sorted(zip(scores, indexes))][:len(input_text) * 2 // 10]
+    low_30 = [x for _, x in sorted(zip(scores, indexes))][:len(input_text) * 5 // 10]
     output = ""
+    previous_mask = False
     for i in range(0, len(input_text)):
-        if i in compare:
-            output = output + " \n " + "[MASK]"
+        if over_limit:
+            break
+        if i in top_30 and dataset_type == "training":
+            if previous_mask:
+                continue
+            else:
+                output = output + " \n " + "[MASK]"
+                total_length += 1
+                previous_mask = True
+        elif i in low_30:
+            continue
         else:
+            token_length = len(word_tokenize(input_text[i]))
+            if total_length + token_length > 1024:
+                over_limit = True
+                break
             output = output + " \n " + input_text[i]
-    return output.strip()
+            total_length += token_length
+            previous_mask = False
+    return output.strip(), total_length, over_limit
 
 
 def generate_index_list(size: int):
@@ -74,9 +95,10 @@ def preprocess(input_path, dataset_type):
 
 
 if __name__ == "__main__":
-    with open("../../data/training_ds.json", "w") as final:
-        json.dump(preprocess("../../data/training.json", "training"), final)
-    with open("../../data/validation_ds.json", "w") as final:
-        json.dump(preprocess("../../data/evaltest.json", "validation"), final)
-    with open("../../data/test_ds.json", "w") as final:
-        json.dump(preprocess("../../data/devtest.json", "test"), final)
+    dataset = dict()
+    dataset["train"] = preprocess("../../data/training.json", "training")
+    dataset["validation"] = preprocess("../../data/evaltest.json", "validation")
+    dataset["test"] = preprocess("../../data/devtest.json", "test")
+    with open("../../data/dataset.json", "w") as final:
+        json.dump(dataset, final)
+
