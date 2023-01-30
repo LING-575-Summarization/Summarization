8d760da388394d55ddb9e7cc671696206c69af08
Junyin Chen <vulpesvulpes825@gmail.com>
2023-01-30 12:03:42 -0800

    feat: Only produce docsetA

--- src/scripts/run_LLM.sh
+++ src/scripts/run_LLM.sh
@@ -8,14 +8,10 @@ conda activate ~/anaconda3/envs/575wi-env
 which python
 
 # Get First JSON via proc_docset
-python src/preprocess/proc_docset.py "/dropbox/22-23/575x/Data/Documents/devtest/GuidedSumm10_test_topics.xml" data/A_devtest --gold_directory "/home2/junyinc/dropbox/22-23/575x/Data/models/devtest/" --no_tokenize
-python src/preprocess/proc_docset.py "/dropbox/22-23/575x/Data/Documents/training/2009/UpdateSumm09_test_topics.xml" data/A_training --gold_directory "/home2/junyinc/dropbox/22-23/575x/Data/models/training/2009/" --no_tokenize
-python src/preprocess/proc_docset.py "/dropbox/22-23/575x/Data/Documents/evaltest/GuidedSumm11_test_topics.xml" data/A_evaltest --gold_directory "/home2/junyinc/dropbox/22-23/575x/Data/models/evaltest/" --no_tokenize
+python src/preprocess/proc_docset.py "/dropbox/22-23/575x/Data/Documents/devtest/GuidedSumm10_test_topics.xml" data/devtest --gold_directory "/home2/junyinc/dropbox/22-23/575x/Data/models/devtest/" --no_tokenize
+python src/preprocess/proc_docset.py "/dropbox/22-23/575x/Data/Documents/training/2009/UpdateSumm09_test_topics.xml" data/training --gold_directory "/home2/junyinc/dropbox/22-23/575x/Data/models/training/2009/" --no_tokenize
+python src/preprocess/proc_docset.py "/dropbox/22-23/575x/Data/Documents/evaltest/GuidedSumm11_test_topics.xml" data/evaltest --gold_directory "/home2/junyinc/dropbox/22-23/575x/Data/models/evaltest/" --no_tokenize
 
-python src/preprocess/proc_docset.py "/dropbox/22-23/575x/Data/Documents/devtest/GuidedSumm10_test_topics.xml" data/B_devtest --gold_directory "/home2/junyinc/dropbox/22-23/575x/Data/models/devtest/" --no_tokenize --docset "B"
-python src/preprocess/proc_docset.py "/dropbox/22-23/575x/Data/Documents/training/2009/UpdateSumm09_test_topics.xml" data/B_training --gold_directory "/home2/junyinc/dropbox/22-23/575x/Data/models/training/2009/" --no_tokenize --docset "B"
-python src/preprocess/proc_docset.py "/dropbox/22-23/575x/Data/Documents/evaltest/GuidedSumm11_test_topics.xml" data/B_evaltest --gold_directory "/home2/junyinc/dropbox/22-23/575x/Data/models/evaltest/" --no_tokenize --docset "B"
 
 # Get Second JSON via LLM preprocess
-python src/LLM/preprocess.py --raw_json_dir "data/A_"
-python src/LLM/preprocess.py --raw_json_dir "data/B_"
+python src/LLM/preprocess.py --raw_json_dir "data/"
