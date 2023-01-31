8f615eafbb231c87a1758cd8af9d708eb4c9910a
Junyin Chen <vulpesvulpes825@gmail.com>
2023-01-30 20:00:05 -0800

    Update Run_D3.sh

--- src/scripts/run_D3.sh
+++ src/scripts/run_D3.sh
@@ -7,9 +7,9 @@ conda activate ~/anaconda3/envs/575wi-env
 
 which python
 
-echo "Running LLM..."
-src/scripts/./run_LLM.sh
 echo "Running LexRank..."
 src/scripts/lexrank.sh --data_path "data" --data_set "devtest" --threshold 0.1 --error 1e-8
 echo "Running Linear Programming..."
 src/scripts/linear_programming.sh outputs/devtest.json 100
+echo "Running LLM..."
+src/scripts/./run_LLM.sh
