488a0b73f2d7c964b4919b8a55d1257d6027015b
Hilly Steinmetz <hsteinm@uw.edu>
2023-01-30 19:58:58 -0800

    stylistic change

--- src/scripts/run_D3.sh
+++ src/scripts/run_D3.sh
@@ -10,6 +10,6 @@ which python
 echo "Running LLM..."
 src/scripts/./run_LLM.sh
 echo "Running LexRank..."
-/nopt/python-3.6/bin/python3 src/content_selection/lexrank.py --data_path "data" --data_set "devtest" --threshold 0.1 --error 1e-8
+src/scripts/lexrank.sh --data_path "data" --data_set "devtest" --threshold 0.1 --error 1e-8
 echo "Running Linear Programming..."
 src/scripts/linear_programming.sh outputs/devtest.json 100
