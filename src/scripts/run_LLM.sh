7701cb2845727dd7e33c03a75b0889adb78ec0ce
Junyin Chen <VulpesVulpes825@gmail.com>
2023-02-20 13:54:27 -0800

    feat: Update run_LLM

--- src/scripts/run_LLM.sh
+++ src/scripts/run_LLM.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+which conda
+
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate ~/anaconda3/envs/575wi-env
+
+which python
+
 # Get First JSON via proc_docset
 python src/preprocess/proc_docset.py "/dropbox/22-23/575x/Data/Documents/devtest/GuidedSumm10_test_topics.xml" data/devtest --gold_directory "/home2/junyinc/dropbox/22-23/575x/Data/models/devtest/" --no_tokenize
 python src/preprocess/proc_docset.py "/dropbox/22-23/575x/Data/Documents/training/2009/UpdateSumm09_test_topics.xml" data/training --gold_directory "/home2/junyinc/dropbox/22-23/575x/Data/models/training/2009/" --no_tokenize
@@ -7,5 +14,5 @@ python src/preprocess/proc_docset.py "/dropbox/22-23/575x/Data/Documents/evaltes
 
 
 # Get Second JSON via LLM preprocess
-python src/LLM/preprocess.py --raw_json_dir "data/"
-python src/LLM/pipeline.py
+python src/LLM/Summarize/preprocess.py --raw_json_dir "data/" --do_mask --do_previous_mask --do_throw --rouge "rouge1"
+python src/LLM/Summarize/pipeline.py
