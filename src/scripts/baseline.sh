6ba2035aa74153c72bcc2285bcaf1cb4938ea195
Anna <readerbatra@gmail.com>
2023-03-01 16:24:46 -0800

    finished baseline and outputs in D5 with .4

---
+++ src/scripts/baseline.sh
@@ -0,0 +1,5 @@
+python3 src/content_selection/baseline.py $1 $2 $3
+
+# 1: json_file path for data: "output/[training/devtest/test].json"
+# 2: max summary length, default is 100
+# 3: output_file directory
