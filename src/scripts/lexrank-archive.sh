660d9610f5e958433121c99ea58b8acf64d74430
Hilly Steinmetz <hsteinm@uw.edu>
2023-03-06 20:23:46 -0800

    fixing

--- src/scripts/lexrank-archive.sh
+++ src/scripts/lexrank-archive.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-python src/run_lexrank-archive.py
+echo "Running LexRank on devtest..."
+./src/scripts/lexrank.sh --data_path data --data_set devtest --min_jaccard_dist 0.7 \
+    --content_realization --idf_docset --ignore_punctuation --lowercase --ignore_stopwords
