96c4c4d9ada1156ceb1bd39437995e179cd9c6ff
Junyin Chen <VulpesVulpes825@gmail.com>
2023-03-06 20:34:03 -0800

    Merge dev to main

--- linear_programming.py
+++ linear_programming.py
@@ -1,9 +1,19 @@
 import sys
+import os
 import pulp
 import json
 from collections import OrderedDict
-# from tf_idf import create_tf_idf_dict
-from we_tried_newtfidf import TFIDF
+from tfidf import TFIDF
+from nltk.util import ngrams
+from sacremoses import MosesDetokenizer
+from clustering import SentenceIndex, create_clusters
+from utils import flatten_list, docset_loader
+
+if __name__ == '__main__':
+    module_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    sys.path.append(module_path)
+
+from content_realization import replace_referents
 
 class LinearProgramSummarizer:
 
@@ -16,34 +26,42 @@ class LinearProgramSummarizer:
                 sent_length,
                 delta_idf,
                 delta_tf,
-                contains_punc,
+                eliminate_punc,
                 lower_casing,
                 log_base
             ):
         self.docset_id = docset_id
-        self.sent_length = sent_length
-        # self.weights = create_tf_idf_dict(json_path, 1, 1)[docset_id]
+        self.data = docset
+
+        # get concept weights here
         self.weights = TFIDF.idf_from_docset(
-            docset,
-            whole_data,
-            contains_punc,
-            lower_casing,
-            "docset",
-            ngram,
-            docset_id,
+            tf_documents=docset,
+            idf_documents=whole_data,
+            punctuation=eliminate_punc,
+            lowercase=lower_casing,
+            doc_level="docset",
+            ngram=ngram,
+            docset_id=docset_id,
             log_tf=log,
             log_idf=log,
             delta_tf=delta_tf,
             delta_idf=delta_idf
         )
-        self.L = max_sum_length
-        self.data = docset
 
+        # hyper-paraemeters
+        # tfidf constraints
+        self.ngram = ngram
+        self.lowercase = lower_casing
+        self.sent_length = sent_length
+        self.L = max_sum_length
+
+        # Initialize LP problem
         self.lp = pulp.LpProblem(name="summarizer", sense=pulp.LpMaximize)
 
         self.sent_decision_vars = []
         self.concept_decision_vars = []
 
+        # Lookup tables
         # look-up list mapping z_i --> (doc_id, term_i)
         self.index_to_concept = []
         self.concept_to_index = {}
@@ -62,17 +80,19 @@ class LinearProgramSummarizer:
 
 
     def make_summary(self):
-
+        """
+            Returns a list of lists, A list of tokenized sentences
+        """
         self.lp.solve()
 
-        summary_str = ""
+        summary_repr = []
         for var in self.sent_decision_vars:
             if pulp.value(var) == 1:
                 index = int(str(var).split("_")[1])
-                sent_repr = self.index_to_sent[index]
-                summary_str += " ".join(sent_repr[1]) + "\n"
+                sent_repr = self.index_to_sent[index][1]
+                summary_repr.append(sent_repr)
 
-        return summary_str
+        return summary_repr
 
 
     def _read_data(self):
@@ -90,9 +110,9 @@ class LinearProgramSummarizer:
             text = doc_data[3]
             for paragraph in text:
                 for sentence in paragraph:
-
-                    # if len(sentence) > self.sent_length:
-                    #     continue
+                    # for sent_length flag
+                    if len(sentence) < self.sent_length:
+                        continue
 
                     y_j = self._make_max_length_weights(doc_id, sentence, sent_weights) # y_j
                     self._make_concept_weights(doc_id, sentence, concept_weights, y_j, A_ij_is_1) # z_n, z_n+1, ..., z_n+m
@@ -154,8 +174,15 @@ class LinearProgramSummarizer:
 
 
     def _make_concept_weights(self, doc_id, sentence, concept_weights, y_j, A_ij_is_1):
+        sentence = list(ngrams(sentence, self.ngram, pad_left=True, pad_right=True, left_pad_symbol='<s>', right_pad_symbol='</s>'))
+
         for term in sentence:
-            doc_id_concept = (doc_id, term.lower())
+
+            # for lowercasing flag
+            if self.lowercase:
+                term = tuple([word.lower() for word in term])
+
+            doc_id_concept = (doc_id, term)
 
             if doc_id_concept not in self.index_to_concept:
                 # initialize each z_i for each term
@@ -170,7 +197,7 @@ class LinearProgramSummarizer:
                 # append (doc_id, term) to lookup table concepts
                 self.index_to_concept.append(doc_id_concept)
 
-                concept_weights[z_i] = self.weights[(term,), self.docset_id]
+                concept_weights[z_i] = self.weights[term, self.docset_id]
             else:  # need to look up z_i in lookup table
                 cur_index = self.concept_to_index[doc_id_concept]
                 z_i = self.concept_decision_vars[cur_index]
@@ -180,40 +207,73 @@ class LinearProgramSummarizer:
 
 def read_json(json_path):
     with open(json_path, "r") as final:
-        read = final.read()
-    docset_rep = json.loads(read)
+        docset_rep = json.load(final)
     return docset_rep
+
+
+def detokenize_summary(summary):
+    detokenizer = MosesDetokenizer()
+    summary_str = ""
+    for sent in summary:
+        summary_str += detokenizer.detokenize(sent) + "\n"
+    return summary_str
 
 
 if __name__ == '__main__':
     # from shell script
     json_path = sys.argv[1]
-    max_summary_length = sys.argv[2]
+    max_summary_length = int(sys.argv[2])
     output_dir = sys.argv[3]
     ngram = int(sys.argv[4])
     sent_length = int(sys.argv[5])
     delta_idf = float(sys.argv[6])
     delta_tf = float(sys.argv[7])
-    contains_punc = bool(sys.argv[8])
+    eliminate_punc = bool(sys.argv[8])
     lower_casing = bool(sys.argv[9])
     log = bool(sys.argv[10])
 
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
     docset_rep = read_json(json_path)
+    fractional_order = SentenceIndex(json_path)
+
+    # create idf_docsset for tf-idf object
+    idf_docset = {}
+    for docset_id, docset in docset_rep.items():
+        for document, data in docset.items():
+            doc_key = docset_id + "." + document
+            idf_docset[doc_key] = data
+
 
     for docset_id, docset in docset_rep.items():
         model = LinearProgramSummarizer(
             docset_id,
             docset,
-            docset_rep,
+            idf_docset,
             max_summary_length,
             ngram,
             sent_length,
             delta_idf,
             delta_tf,
-            contains_punc,
-            lower_casing
+            eliminate_punc,
+            lower_casing,
+            log
         )
+        # gets unordered summary
         summary = model.make_summary()
+
+        # order the summary
+        summary = create_clusters(docset_id, summary, fractional_order, json_path)
+
+        # content realization
+        docset_2, indices = docset_loader(json_path, docset_id)
+        summary = replace_referents(summary, docset_2)
+
+        # detokenizer
+        summary = detokenize_summary(summary)
+
+        # output summaries to correct file
         docset_id = docset_id.split("-")[0]
         id_part1 = docset_id[:-1]
         id_part2 = docset_id[-1]
@@ -221,4 +281,3 @@ if __name__ == '__main__':
         with open(output_summary_file, 'w') as output:
             output.write(summary)
 
-        break
